9bd5830a4eb856e2efcda0b6f2c8b6ab810d5629
mveluru <mveluru@gmail.com>
2024-04-17 16:11:30 -0500

    Initial commit

---
+++ src/main/java/com/example/restapiversionformats/RestApiversionformatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapiversionformats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiversionformatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiversionformatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restapiversionformats/RestApiversionformatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapiversionformats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiversionformatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
